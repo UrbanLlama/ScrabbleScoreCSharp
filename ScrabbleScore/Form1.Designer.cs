6ec41a5bea083eb69dc2dc5700a26c5dfe48e239
UrbanLlama <dmytrello@yahoo.com>
2018-09-24 14:05:03 +0300

    Initial commit.

--- ScrabbleScore/Form1.Designer.cs
+++ ScrabbleScore/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ScrabbleScore
 {
-    partial class Form1
+    partial class fMain
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,53 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblScore.Location = new System.Drawing.Point(14, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(376, 30);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score: 0";
+            this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtInput
+            // 
+            this.txtInput.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtInput.Location = new System.Drawing.Point(12, 52);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(375, 30);
+            this.txtInput.TabIndex = 1;
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(402, 94);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblScore);
+            this.MaximizeBox = false;
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ScrabbleScore";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtInput;
     }
 }
 
