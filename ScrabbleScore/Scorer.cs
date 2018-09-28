5d0c6d922ed54cb25ce85ae1f3512b7bb0906c29
UrbanLlama <dmytrello@yahoo.com>
2018-09-28 12:40:44 +0300

    Added proper letter scoring according to Enlglish language.

--- ScrabbleScore/Scorer.cs
+++ ScrabbleScore/Scorer.cs
@@ -10,22 +10,22 @@
 
             for (int n = 0; n < word.Length; n++)
             {
-                if (word[n] == 2 && n == word.Length)
+                if ((word[n] == '2') && (n == word.Length -1))
                 {
                     score *= 2;
                 }
                 else
-                if (word[n] == 3 && n == word.Length)
+                if ((word[n] == '3') && (n == word.Length - 1))
                 {
                     score *= 3;
                 }
                 else
-                    if (word[n] == 2)
+                    if (word[n] == '2')
                 {
                     modifier = 2;
                 }
                 else
-                if (word[n] == 3)
+                if (word[n] == '3')
                 {
                     modifier = 3;
                 }
@@ -42,8 +42,19 @@
 
         public int GetLetterScore(char letter)
         {
-            //TODO добавить алфавиты и вычисление стоимости буквы
-            return 0;
+            char[] letters = {'E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U',
+                                    'D', 'G', 'B', 'C', 'M', 'P', 'F', 'H', 'V', 'W', 'Y',
+                                    'K', 'J', 'X', 'Q', 'Z'};
+
+            int[] scores = {1,1,1,1,1,1,1,1,1,1, 2,2, 3,3,3,3, 4,4,4,4,4, 5, 8,8, 10,10};
+
+            int index = 0;
+            do
+            {
+                index += 1;
+            } while (letters[index] != letter);
+
+            return scores[index];
         }
     }
 }
